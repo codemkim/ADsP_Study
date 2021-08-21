9da06a5bdd1e78c256e893682380d0ff44b94d93
codemkim <inkin61@naver.com>
2021-08-21 22:47:15 +0900

    1

---
+++ R_learning_02.R
@@ -0,0 +1,38 @@
+#스칼라 값 만들기
+rm(list=ls())
+iv = c(1,2,3)
+cv = c('a','b','c')
+bv = c(TRUE,FALSE,TRUE)
+fv = c(3.4, 2.5, 8)
+t = c(1,2,3,4)
+icv = c(iv,cv,bv)
+
+
+#시작 끝 출력 
+rm(list=ls())
+v1 = seq(1,5)
+v2 = seq(1,10,2)
+v3 = seq(1,10, length=4)
+v4 = seq(1,3, length=5)
+
+# 시작값 반복
+rm(list=ls())
+v1=rep(1,5)
+v2=rep(1,time=5)
+v3=rep(3:5, 3)
+v4=rep(3:5, each=2)
+v5=rep(seq(1,5), each=2)
+
+
+v6=rep(seq(1,5), each=2)
+
+#시작 끝 출력 
+rm(list=ls())
+v1 = seq(1,5)
+v2 = seq(1,10,2)
+v3 = seq(1,10, length=4)
+v4 = seq(1,3, length=5)
+
+
+v3 = seq(1,4)
+
