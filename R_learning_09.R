4df8351ed51abab1af5494fa6644e5e99582eb15
codemkim <inkin61@naver.com>
2021-08-22 14:29:34 +0900

    6

---
+++ R_learning_09.R
@@ -0,0 +1,26 @@
+x =c(1,2,3,4)
+y =c('a','b','c')
+
+xy= c(x,y)
+xy
+
+y = c(1,2,3,NA)
+
+3*y
+
+
+a = c(5,10,1,2)
+
+rm(list=ls())
+x = 1:5
+y = seq(10,50,10)
+
+
+a = rbind(x,y)
+
+a[1,]
+
+
+summary(a)
+str(a)
+head(a)
