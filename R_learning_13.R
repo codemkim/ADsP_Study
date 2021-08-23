165d424bf970d60dd5169b372613f574492ffd71
codemkim <inkin61@naver.com>
2021-08-23 16:53:44 +0900

    4

---
+++ R_learning_13.R
@@ -0,0 +1,15 @@
+rm(list=ls())
+
+a = 1:5
+b = c('a','b','c')
+
+paste(a,b)
+
+c = c('apple','banana','orange')
+substr(c,2,4)
+
+r1 = split(iris, iris$Species)
+
+subset(iris, subset = Species== 'setosa' & Sepal.Length > 5.5)
+
+subset(iris, select = c(Sepal.Length, Species))
